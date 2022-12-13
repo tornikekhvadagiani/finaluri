4c95e365f394242b158e173bfd78dcc3a48d66a7
tornikekhvadagiani <113547924+tornikekhvadagiani@users.noreply.github.com>
2022-12-13 23:48:10 +0400

    dasasruli

--- script.js
+++ script.js
@@ -1,195 +1,106 @@
-let slideIndex = 0;
 showSlides();
+initRecommendationSlider();
+initFilterProjects();
 
 function showSlides() {
+  let slideIndex = 0;
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); 
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000);
 }
 
+const autoProgressBar = document.querySelector(".autoProgressBar");
+const Design = document.getElementById("Design");
+const Photography = document.getElementById("Photography");
+const Marking = document.getElementById("Marking");
+const Photoshop = document.getElementById("Photoshop");
 
+window.addEventListener("scroll", () => {
+  if (autoProgressBar.getBoundingClientRect().top <= 500) {
+    Design.style.width = 80 + "%";
+    Photography.style.width = 50 + "%";
+    Marking.style.width = 65 + "%";
+    Photoshop.style.width = 30 + "%";
+  }
+});
 
-// first section 
-const slideShow = document.querySelectorAll('.hide');
-let currentImageCounter = 0;
+function initFilterProjects() {
+  const filters = document.querySelectorAll(".bar-li");
+  const projects = document.querySelectorAll(".project");
 
-slideShow[currentImageCounter].style.display ="block";
-   setInterval(nextImage, 5000);
+  projects.forEach((project) => {
+    project.style.display = "block";
+  });
 
-function nextImage(){
-    slideShow[currentImageCounter].style.display ="none";
-    currentImageCounter= (currentImageCounter + 1)%(slideShow.length);
-    slideShow[currentImageCounter].style.display ="block";
-}
+  filters.forEach((filter) => {
+    filter.addEventListener("click", () => {
+      const name = filter.dataset.name;
 
-const skills = document.querySelector('.skills');
-const Design = document.getElementById('Design');
-const Photography = document.getElementById('Photography');
-const Marking = document.getElementById('Marking');
-const Photoshop = document.getElementById('Photoshop');
-
-window.addEventListener('scroll', () => {
-    if(skills.getBoundingClientRect().top <= 400) {
-        Design.style.width = 80 + "%"
-        Photography.style.width = 50 + "%"
-        Marking.style.width = 65 + "%"
-        Photoshop.style.width = 30 + "%"
-    }
-})
-
-
-// section four slide with buttons
-
-const firstButton = document.querySelector('.slide-btn-first');
-const secondButton = document.querySelector('.slide-btn-second');
-const thirdButton = document.querySelector('.slide-btn-third');
-
-const firstReview = document.getElementById('review-1');
-const secondReview = document.getElementById('review-2');
-const thirdReview = document.getElementById('review-3');
-
-secondButton.addEventListener('click', ()=>{
-    secondReview.classList.remove('none');
-    firstReview.classList.add('none');
-    thirdReview.classList.add('none');
-})
-
-firstButton.addEventListener('click',()=>{
-    firstReview.classList.remove('none');
-    secondReview.classList.add('none');  
-    thirdReview.classList.add('none');
-})
-
-thirdButton.addEventListener('click', ()=>{
-    thirdReview.classList.remove('none');
-    firstReview.classList.add('none');
-    secondReview.classList.add('none');  
-})
-
-
-// send user to server 
-
-const regForm = document.getElementById('reg-form');
-const userName = document.getElementById('name');
-const userEmail = document.getElementById('email');
-const userUrl = document.getElementById('url');
-const userMessage = document.getElementById('user-message');
-
-const regButton = document.getElementById('submit');
-const modal = document.querySelector('.block');
-const modalButton = document.querySelector('.modal-btn');
-
-
-function addNewUser(info) {
-    fetch("https://borjomi.loremipsum.ge/api/send-message", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(info),
-    })
-        .then((res) => res.json())
-        .then((data) => {
-          
-        })
-       
-       
-        .catch((err) => {
-            console.log(err);
+      if (name === "all") {
+        projects.forEach((project) => {
+          project.style.display = "block";
         });
+      } else {
+        projects.forEach((project) => {
+          project.style.display = "none";
+        });
+        const currentProject = Array.from(projects).find(
+          (project) => project.dataset.name === name
+        );
+
+        currentProject.style.display = "block";
+      }
+    });
+  });
 }
 
+function initRecommendationSlider() {
+  const slides = document.querySelectorAll(".recommendation-position");
+  const dots = document.querySelectorAll(".buttons");
 
-
-regForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-
-    const userNameValue = userName.value   
-    const userEmailValue = userEmail.value
-    const userUrlValue = userUrl.value
-    const userMessageValue = userMessage.value
-
-
-
-    const user = {
-        Name:userNameValue,
-        Email:userEmailValue,
-        website: userUrlValue,
-        message:  userMessageValue
-    }
-    console.log(user);
-    
-    buttonsEvent();
-    addNewUser(user);
- 
-     regForm.reset();
-    
-})
-
-function buttonsEvent(){
-
-        modal.classList.remove('block');
-       
-       modalButton.addEventListener('click',()=>{
-         modal.classList.add('block');
-       })
+  activateFirstItem(slides, dots);
+  addClickListener(slides, dots);
 }
 
+function addClickListener(slides, dots) {
+  dots.forEach((dot) => {
+    dot.addEventListener("click", () => {
+      const index = dot.dataset.index;
 
-
-
-const navLi = document.querySelectorAll('.nav-li');
-const frontImage = document.querySelectorAll('.front-project');
-const background = document.querySelectorAll('.title-background');
-
-navLi.forEach((e) => 
-    e.addEventListener('click', (li) =>{
-        let clickedId = li.target.getAttribute('id');
-        if(clickedId != "allid"){
-            showCurrentImage(clickedId);
-        } else {
-            showAllImages();
-        }
-    })
-)
-     
-function showCurrentImage(itemId){
-
-    let activeImage = itemId + "-img";
-
-     frontImage.forEach((el) =>{
-        let image = el.getAttribute('id');
-          if(activeImage != image){
-            el.classList.add('display')
-          } else {
-            el.classList.remove('display');
-          }
-     })
-
-     background.forEach((e) => {
-        e.classList.remove('title-background');
-        e.classList.add('block');
-     })
+      activateDot(dots, index);
+      activateSlide(slides, index);
+    });
+  });
 }
 
-function showAllImages(){
-    frontImage.forEach((el) =>{
-        el.classList.remove('display');
-    })
-    background.forEach((e) => {
-        e.classList.remove('block');
-        e.classList.add('title-background');
-       
-     })
+function activateFirstItem(slides, dots) {
+  dots[0].classList.add("active");
+  slides[0].style.display = "flex";
 }
 
+function activateDot(dots, index) {
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  dots[index].classList.add("active");
+}
 
+function activateSlide(slides, index) {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "flex";
+}
